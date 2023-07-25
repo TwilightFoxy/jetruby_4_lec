dbfd3e78ed6d475b439b10ac09d0b967b1bb2c07
Levon Mikhaelyan <twilightfoxy@icloud.com>
2023-07-25 12:40:36 +0300

    Check is word ending CS

---
+++ isCS.rb
@@ -0,0 +1,15 @@
+def check(word)
+  word.downcase.end_with?("cs")
+end
+
+def is_cs(word)
+  if check(word)
+    puts 2 ** word.length
+  else
+    puts word.reverse
+  end
+end
+
+# Пример использования
+# is_cs("RubyCS")
+# is_cs("Ruby")
